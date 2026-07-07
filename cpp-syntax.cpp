46d6b7dad97bea53603176ea9b9b21aa9bd577ca
“Username” <valentinusnbbn2@student.ub.ac.id>
2026-07-07 22:24:16 +0700

    Create cpp-syntax.cpp

---
+++ cpp-syntax.cpp
@@ -0,0 +1,26 @@
+//1. Syntax
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!";
+    return 0;
+}
+//Ommiting Namespace
+int main(){
+    std::cout << "Hello World!";
+    return 0;
+}
+
+//2. Statement
+int main(){
+    std::cout << "Hello Guys";
+    std::cout << "Have a nice day";
+    return 0;
+}
+
+//3. Creating a Simple C++ Program
+int main(){
+    cout << "Hello C++!";
+    return 0;
+}
