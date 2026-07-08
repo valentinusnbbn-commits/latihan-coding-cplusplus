b70b7d3a4e8c0572e628439ed96a6d836692cabc
“Username” <valentinusnbbn2@student.ub.ac.id>
2026-07-08 18:52:09 +0700

    Create cpp-output.cpp

---
+++ cpp-output.cpp
@@ -0,0 +1,29 @@
+//1. Print Text
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!";
+    std::cout <<"I'm learning C++.";
+    return 0;
+}
+
+//2. Print Numbers
+int main(){
+    cout << 5 * 8;
+    return 0;
+}
+
+//3. New Lines
+int main(){
+    cout << "Hello World !\n";
+    std::cout << "I'm Learning C++";
+    return 0;
+}
+
+//4. Code Challenge
+int main(){
+    cout << "Valentinus Dian Nababan";
+    cout << 19;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+
+// int main(){
+//     cout << "Hello World!";
+//     std::cout <<"I'm learning C++.";
+//     return 0;
+// }
+
+// //2. Print Numbers
+// int main(){
+//     cout << 5 * 8;
+//     return 0;
+// }
